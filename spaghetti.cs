7636e6093a0a7fb1cb6806816ae4a527a7146de2
IdkGoodName <31628630+CreatorVilius@users.noreply.github.com>
2020-07-30 16:26:05 +0300

    Spaghetti
    Spaghetti

---
+++ spaghetti.cs
@@ -0,0 +1,13 @@
+// Spaghetti
+using System.Spaghettify;
+
+namespace Spaghetties {
+  public interface Food {
+    void Eat();
+  }
+  public class Spaghetti: Food {
+    public void Eat() {
+      // Spaghetti
+    }
+  }
+}
